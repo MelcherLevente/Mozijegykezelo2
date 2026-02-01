ea5491597525c713afbe6d640d9703ef502e3ce1
MelcherLevente <melcherlevente@turr.hu>
2026-02-01 23:45:15 +0100

    Film class

--- Model/Film.cs
+++ Model/Film.cs
@@ -8,5 +8,24 @@ namespace Mozijegykezelo1.Model
 {
     internal class Film
     {
+        public Film(int id, string cim, string kategoria, string rendezo, int megjelenesEve, int hossz)
+        {
+            Id = id;
+            Cim = cim;
+            Kategoria = kategoria;
+            Rendezo = rendezo;
+            MegjelenesEve = megjelenesEve;
+            Hossz = hossz;
+        }
+        public Film()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Cim { get; set; }
+        public string Kategoria { get; set; }
+        public string Rendezo { get; set; }
+        public int MegjelenesEve { get; set; }
+        public int Hossz { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,46 @@
-﻿using Mozijegykezelo1.Model;
+﻿using Mozijegykezelo1.Document;
+using Mozijegykezelo1.Model;
+using System.Data;
 
 internal class Program
 {
     public static FileIO.ReadFromFile reader = new FileIO.ReadFromFile();
     public static List<List<string>> jegyAdatok = new();
     public static List<Jegy> jegyek;
+
+    public static readonly string connectionString = "Server=localhost;Database=mozi;User=root;";
+    public static DataTable filmAdatok = new DataTable();
+    public static List<Film> filmLista = new List<Film>();
     static void Main(string[] args)
     {
         JegyBeolvasas(ref jegyAdatok);
         JegyFeltoltes(jegyAdatok);
         Kiiras(jegyek);
+
+        //DBCheck(connectionString);
+        SelectFromTable("filmek", connectionString);
+        FilmFeltoltes(filmAdatok);
+        PrintDataTable(filmAdatok);
+
+    }
+
+    private static void PrintDataTable(DataTable filmLista)
+    {
+        foreach (DataColumn column in filmLista.Columns)
+        {
+            Console.Write(column.ColumnName + "\t");
+        }
+        Console.WriteLine();
+
+        // Print rows
+        foreach (DataRow row in filmLista.Rows)
+        {
+            foreach (var item in row.ItemArray)
+            {
+                Console.Write(item + "\t");
+            }
+            Console.WriteLine();
+        }
     }
 
     private static void JegyBeolvasas(ref List<List<string>> jegyAdatok)
@@ -39,5 +70,34 @@ internal class Program
         {
             Console.WriteLine(jegy.ToString());
         }
+    }
+
+
+
+    private static void FilmFeltoltes(DataTable filmAdatok)
+    {
+        foreach (DataRow f in filmAdatok.Rows)
+        {
+            Film film = new Film();
+            film.Id = f.Field<int>(0);
+            film.Cim = f.Field<string>(1);
+            film.Kategoria = f.Field<string>(2);
+            film.Rendezo = f.Field<string>(3);
+            film.MegjelenesEve = f.Field<int>(4);
+            film.Hossz = f.Field<int>(5);
+
+            filmLista.Add(film);
+        }
+    }
+
+    private static void SelectFromTable(string tableName, string connectionString)
+    {
+        filmAdatok = DatabaseService.GetAllData(tableName, connectionString);
+        Console.WriteLine("Adatok sikeresen szinkronizálva az adatbázisból, átmeneti tárolóba");
+    }
+
+    private static void DBCheck(string connectionString)
+    {
+        DatabaseService.DbConnectionCheck(connectionString);
     }
 }
