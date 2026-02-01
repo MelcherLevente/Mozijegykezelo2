f408ebb8b7faa6a857e6c6b5e0b45c9ff0f67a78
MelcherLevente <melcherlevente@turr.hu>
2026-02-01 22:03:56 +0100

    Jegy class

---
+++ Model/Jegy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mozijegykezelo1.Model
+{
+    internal class Jegy
+    {
+        private string _vevoNev;
+        private string _filmCim;
+        private string _vetitesIdopont;
+        private string _szekSor;
+        private int _szekSzam;
+
+        public Jegy(string vevoNev, string filmCim, string vetitesIdopont, string szekSor, int szekSzam)
+        {
+            _vevoNev = vevoNev;
+            _filmCim = filmCim;
+            _vetitesIdopont = vetitesIdopont;
+            _szekSor = szekSor;
+            _szekSzam = szekSzam;
+        }
+
+        public Jegy()
+        {
+
+        }
+
+        public string VevoNev { get => _vevoNev; set => _vevoNev = value; }
+        public string FilmCim { get => _filmCim; set => _filmCim = value; }
+        public string VetitesIdopont { get => _vetitesIdopont; set => _vetitesIdopont = value; }
+        public string SzekSor { get => _szekSor; set => _szekSor = value; }
+        public int SzekSzam { get => _szekSzam; set => _szekSzam = value; }
+
+        public override string ToString()
+        {
+            return $"{VevoNev}, {FilmCim}, {VetitesIdopont}, {SzekSor} sor, {SzekSzam}. szék";
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Mozijegykezelo1.Model;
+
+internal class Program
+{
+    public static FileIO.ReadFromFile reader = new FileIO.ReadFromFile();
+    public static List<List<string>> jegyAdatok = new();
+    public static List<Jegy> jegyek;
+    static void Main(string[] args)
+    {
+        JegyBeolvasas(ref jegyAdatok);
+        JegyFeltoltes(jegyAdatok);
+        Kiiras(jegyek);
+    }
+
+    private static void JegyBeolvasas(ref List<List<string>> jegyAdatok)
+    {
+        jegyAdatok = reader.FileRead("Jegyek.csv", 5, ',', true);
+    }
+    private static void JegyFeltoltes(List<List<string>> jegyAdatok)
+    {
+        List<List<string>> jegyLista = jegyAdatok;
+        jegyek = new List<Jegy>();
+
+        foreach (var item in jegyLista)
+        {
+            string vevoNev = item[0];
+            string filmCim = item[1];
+            string vetitesIdopont = item[2];
+            string szekSor = item[3];
+            int szekSzam = int.Parse(item[4]);
+
+            Jegy j = new Jegy(vevoNev, filmCim, vetitesIdopont, szekSor, szekSzam);
+            jegyek.Add(j);
+        }
+    }
+    private static void Kiiras(List<Jegy> jegyek)
+    {
+        foreach (var jegy in jegyek)
+        {
+            Console.WriteLine(jegy.ToString());
+        }
+    }
+}
